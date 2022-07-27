1fa6015fd6331dbfdcee7566dbdb6a4f3470f6b4
Jaydeep Joshi <jaydeepmjoshi@gmail.com>
2022-07-27 20:32:01 +0530

    Solutions for 01

--- AOC2021/01/Puzzle01.swift
+++ AOC2021/01/Puzzle01.swift
@@ -6,12 +6,31 @@
 //
 
 struct Puzzle01: Puzzle {
+    private let depths: [Int]
+
+    init() {
+        depths = InputFileReader.read("Input01").compactMap(Int.init)
+    }
 
     func part1() {
-        
+        var increasing = 0
+        for i in (1..<depths.endIndex) {
+            let diff = depths[i] - depths[i - 1]
+            if diff > 0 {
+                increasing += 1
+            }
+        }
+        print(increasing)
     }
 
     func part2() {
-
+        var increasing = 0
+        for i in (1..<depths.endIndex-2) {
+            let diff = (depths[i]+depths[i+1]+depths[i+2]) - (depths[i-1]+depths[i]+depths[i+1])
+            if diff > 0 {
+                increasing += 1
+            }
+        }
+        print(increasing)
     }
 }
