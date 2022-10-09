43ff03944b250130ece6083660a9b3bd5e7e4252
Jaydeep Joshi <jaydeepmjoshi@gmail.com>
2022-10-09 15:43:01 +0530

    Skip 16
    Packet parsing is proving a bit boring.

--- AOC2021/16/Puzzle16.swift
+++ AOC2021/16/Puzzle16.swift
@@ -72,14 +72,40 @@ struct Puzzle16: Puzzle {
     }
 
     private func parsePacket(offset: inout Int) -> Packet {
-        let version = Int(String(input[offset..<offset+3]), radix: 2)!
-        let id = Int(String(input[offset+3..<offset+6]), radix: 2)!
+        let version = binary2dec(input[offset..<offset+3])
+        let id = binary2dec(input[offset+3..<offset+6])
+        print("Parsed v\(version), d\(id) @ offset \(offset)")
         offset += 6
         if id == 4 {
             let value = parseLiteralValue(offset: &offset)
             return Packet(version: version, id: id, value: value)
         } else {
-            fatalError("ID \(id) not yet supported")
+            let lengthTypeId = input[offset]
+            offset += 1
+            if lengthTypeId == "0" {
+                let subpacketBits = binary2dec(input[offset..<offset+15])
+                offset += 15
+                var subpackets = [Packet]()
+                let subpacketsStartOffset = offset
+                var parsedOffset = subpacketsStartOffset
+                while parsedOffset != subpacketsStartOffset + subpacketBits {
+                    subpackets.append(parsePacket(offset: &offset))
+                    parsedOffset = offset
+                }
+                return Packet(version: version, id: id, subpackets: subpackets)
+            } else if lengthTypeId == "1" {
+                let subpacketCount = binary2dec(input[offset..<offset+11])
+                offset += 11
+                var subpackets = [Packet]()
+                var parsedCount = 0
+                while parsedCount != subpacketCount {
+                    subpackets.append(parsePacket(offset: &offset))
+                    parsedCount += 1
+                }
+                return Packet(version: version, id: id, subpackets: subpackets)
+            } else {
+                fatalError("Invalid length type ID : \(lengthTypeId)")
+            }
         }
     }
 
@@ -87,12 +113,18 @@ struct Puzzle16: Puzzle {
         var endOffset = offset
         repeat {
             endOffset += 5
-        } while input[endOffset] != "0"
+        } while input.indices.contains(endOffset) && input[endOffset] != "0"
         var value = ""
-        for offset in stride(from: offset, to: endOffset+1, by: 5) {
-            value.append(contentsOf: input[offset+1..<offset+5])
+        for o in stride(from: offset, to: endOffset + 1, by: 5) {
+            if input.indices.contains(o+1), input.indices.contains(o+4) {
+                value.append(contentsOf: input[o+1...o+4])
+            }
         }
         offset = endOffset
-        return Int(String(value), radix: 2)!
+        return Int(value, radix: 2)!
+    }
+
+    private func binary2dec(_ binary: ArraySlice<Character>) -> Int {
+        Int(String(binary), radix: 2)!
     }
 }
